c7f6a27b8fa8324476653c6eadadc7d49fedcd76
Isaac799 <mail@isaaclewis.me>
2025-05-10 14:51:27 -0500

    rm placeholder main code
    main fn was just a proof of concept. Moving towards next idea it is no longer needed.

--- cmd/focustime/focustime.go
+++ cmd/focustime/focustime.go
@@ -3,7 +3,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/Isaac799/focus-time/internal/focusedwindow"
@@ -15,10 +14,6 @@ import (
 const (
 	// Exit exits the program
 	Exit = iota + 1
-	// Upsert adds 1 second to tracked time
-	Upsert
-	// Read triggers a read from the sqlite database
-	Read
 )
 
 func main() {
@@ -34,28 +29,15 @@ func main() {
 
 	options := []string{
 		"Exit",
-		"Upsert",
-		"Read",
 	}
 
-	sqlite.New()
+	sqlite.Init()
 
 	for {
 		i := prompt.Select(options...)
 		switch i {
 		case Exit:
 			os.Exit(0)
-		case Upsert:
-			sqlite.Upsert(title, 1)
-		case Read:
-			records, err := sqlite.Read()
-			if err != nil {
-				log.Fatal(err)
-				return
-			}
-			for _, r := range records {
-				fmt.Printf("%d: %s", r.Seconds, r.Title)
-			}
 		}
 	}
 }
